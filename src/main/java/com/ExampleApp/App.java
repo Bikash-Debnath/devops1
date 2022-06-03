8c5a91f871e7637302505933c3abb02c35369945
Bikash-Debnath <raymon.devnath@gmail.com>
2022-06-03 08:37:13 +0530

    First Commit

---
+++ src/main/java/com/ExampleApp/App.java
@@ -0,0 +1,13 @@
+package com.ExampleApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
